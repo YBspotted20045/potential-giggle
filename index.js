ee76fd2839519634b932d1e775efa4392b5e1d49
YBspotted20045 <ksomtochukwu38@gmail.com>
2025-06-30 17:56:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Replace this with your actual MongoDB URI
+const mongoURI = "mongodb+srv://Ybspotted:SCa20045@growrich-cluster.cduooko.mongodb.net/?retryWrites=true&w=majority&appName=growrich-cluster";
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB error:', err));
+
+app.get('/', (req, res) => {
+  res.send('GrowRich Backend is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
